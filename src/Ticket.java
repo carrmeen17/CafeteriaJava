6004fa3616d43173ce8a2210f506ddd8dd9ec99d
carrmeen17 <carrmeen99@gmail.com>
2026-08-26 17:57:21 +0200

    Hemos continuado con la creación del ticket con sus métodos correspondientes y hemos añadido otro método a la interfaz descontable

--- src/Ticket.java
+++ src/Ticket.java
@@ -23,11 +23,37 @@ public class Ticket {
 
     }
 
+    public void total(){
+
+        double total = 0;
+        for (Producto producto : productos) {
+            total = producto.getPrecio() + total;
+        }
+        System.out.println("Total: " + total + "€. ");
+    }
+
+    public void mostrarDescuentos(){
+        for (Producto producto : productos) {
+            if (producto.getDescuento() > 0){
+                System.out.println("Descuento aplicado al " +producto.nombre +": " +producto.getDescuento() +"%");
+                System.out.println("Precio final del " +producto.nombre + ":" + producto.aplicarDescuento() +"€");
+            }
+
+        }
+
+
+    }
+
     public void mostrarTicket(){
         System.out.println("--- CAFETERÍA JAVA ---");
         System.out.println( "Cliente: " +cliente.getNombre());
         System.out.println("Camarero: " +camarero.getNombre() +" - " +camarero.getCodigoEmpleado());
-
+        System.out.println(" ");
+        recorrerProductos();
+        System.out.println(" ");
+        total();
+        System.out.println(" ");
+        mostrarDescuentos();
 
 //TODO MEJORAR
     }
